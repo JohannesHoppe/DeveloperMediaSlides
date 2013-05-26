d7af4cf9311d76b5d5fa9c986702935d868d2d38
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-05-27 00:40:06 +0200

    fixing sass cmd

--- ! compile sass themes.cmd
+++ ! compile sass themes.cmd
@@ -4,14 +4,14 @@
 CD "%~dp0"
 
 @echo Downloading Chocolatey
-rem @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))"
-rem SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))"
+SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 @echo Downloading Ruby
-rem CALL cinst ruby
+CALL cinst ruby
 
 @echo Downloading Sass
-rem CALL gem install sass
+CALL gem install sass
 
 @echo Starting Sass
 sass --watch css/theme/source/:css/theme --style expanded
