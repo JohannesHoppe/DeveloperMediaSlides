3f4d903c94901ceef15488af10114dd03d504414
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-05-27 00:39:35 +0200

    first content

--- ! compile sass themes.cmd
+++ ! compile sass themes.cmd
@@ -4,14 +4,14 @@
 CD "%~dp0"
 
 @echo Downloading Chocolatey
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))"
-SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+rem @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))"
+rem SET PATH=%PATH%;%systemdrive%\chocolatey\bin
 
 @echo Downloading Ruby
-CALL cinst ruby
+rem CALL cinst ruby
 
 @echo Downloading Sass
-CALL gem install sass
+rem CALL gem install sass
 
 @echo Starting Sass
 sass --watch css/theme/source/:css/theme --style expanded
